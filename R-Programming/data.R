390d55a9d4e4d4c01af23e9a8d016b76f9d544e7
Varun Juyal <varunjuyal123@yahoo.com>
2015-08-09 17:26:00 +0530

    R-Programming practise

---
+++ R-Programming/data.R
@@ -0,0 +1,6 @@
+a <-
+structure(list(a = 1, b = 2), .Names = c("a", "b"), row.names = c(NA, 
+-1L), class = "data.frame")
+test <-
+structure(list(a = 1, b = 2), .Names = c("a", "b"), row.names = c(NA, 
+-1L), class = "data.frame")
